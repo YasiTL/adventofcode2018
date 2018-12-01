26dab6060ff9a54c96bc800f73d62c5efe6cf6ab
YasiTL <yasaman.most@gmail.com>
2018-12-01 01:35:14 -0600

    first commit

---
+++ day1.py
@@ -0,0 +1,26 @@
+import sys
+from collections import Counter
+from itertools import cycle
+
+
+mylist = []
+with open('input.txt') as f:
+    mylist = f.read().splitlines()
+
+total = 0
+sumlist = []
+total2 = 0
+
+
+for num in mylist:
+    total += int(num)
+
+print(total)
+
+for num in cycle(mylist):
+    total2 += int(num)
+    if total2 in sumlist:
+        print(total2)
+        break
+    else:
+        sumlist.append(total2)
