b3e2a434fa565e98e4da01291a75dbe7f893cd72
YasiTL <yasaman.most@gmail.com>
2018-12-03 13:54:58 -0600

    day 3 part 2

--- day3.py
+++ day3.py
@@ -1,41 +1,43 @@
-from array import *
-
 mylist = []
 with open('input3.txt') as f:
     mylist = f.read().splitlines()
 
 claiminfo = []
 fabric = []
-
-fabric = []
-
 fabric = [[0 for i in range(1000)] for i in range(1000)]
-overlap = 0
 
-print(fabric)
+overlap = 0
+idoverlaps = set()
+ids = set()
+
 
 for claim in mylist:
     claiminfo = claim.replace('#', ' ').replace('@',' ').replace(',',' ').replace(':',' ').replace('x', ' ').split()
-    # lmao this doesnt work anymore
-    print(claiminfo)
     id = int(claiminfo[0])
     p2 = int(claiminfo[1])
     p1 = int(claiminfo[2])
     w = int(claiminfo[3])
     h = int(claiminfo[4])
+    ids.add(id)
+
     for i in range(w):
         if fabric[p1][p2+i] != id and fabric[p1][p2+i] != 0:
-            fabric[p1][p2+i] = 'X'
+            idoverlaps.add(fabric[p1][p2+i])
+            fabric[p1][p2+i] = id
+            idoverlaps.add(id)
         else:
             fabric[p1][p2+i] = id
 
         for j in range(h):
             if fabric[p1+j][p2+i] != id and fabric[p1+j][p2+i] != 0:
-                fabric[p1+j][p2+i] = 'X'
-            else:
-                fabric[p1+j][p2+i] = 1
+                idoverlaps.add(fabric[p1+j][p2+i])
+                fabric[p1+j][p2+i] = id
+                idoverlaps.add(id)
 
-print(fabric)
+            else:
+                fabric[p1+j][p2+i] = id
+    
+
 
 for row in fabric:
     for elem in row:
@@ -43,4 +45,5 @@ for row in fabric:
             overlap += 1
 
 print(overlap)
-
+print(len(ids))
+print('PART 2:' + str(ids-idoverlaps))
